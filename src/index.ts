44c50862677e1158565485976119703591126bf1
Lean <basedlainpilled@gmail.com>
2023-05-10 20:10:52 -0400

    clean up

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ import {
   struct,
 } from "thyseus";
 
-/**
- * System that despawns and spawns in entities.
- */
 function systemA(commands: Commands, entities: Query<Entity>) {
   //! After a few iterations, the length of the query will be 0 - why?
   console.log(entities.length);
@@ -22,17 +19,13 @@ function systemA(commands: Commands, entities: Query<Entity>) {
 
   commands.spawn();
 }
-
 systemA.parameters = [CommandsDescriptor(), QueryDescriptor(Entity)];
 
-// Create world
 const world = await World.new().addSystems(systemA).build();
-
 world.runSchedule(CoreSchedule.Startup);
 
 const loop = async () => {
   world.runSchedule(CoreSchedule.Main);
   requestAnimationFrame(loop);
 };
-
 requestAnimationFrame(loop);
